d3fe9e071f17488406f942316b1ebf901c4957cc
KhanhNG <khanhngn.dev@gmail.com>
2023-05-17 21:47:30 +0700

    Setup configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+//theme
+import 'primereact/resources/themes/tailwind-light/theme.css';
+//core
+import 'primereact/resources/primereact.min.css';
+//icons
+import 'primeicons/primeicons.css';
+//main
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<BrowserRouter>
+			<QueryClientProvider client={client}>
+				<App />
+			</QueryClientProvider>
+		</BrowserRouter>
+	</React.StrictMode>
+);
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{ts,tsx,js,jsx}'],
+	theme: {
+		extend: {
+			backgroundColor: {
+				primary: '#4F46E5',
+				secondary: '#304CE0',
+				action: '#E03030',
+			},
+			colors: {
+				primary: '#4F46E5',
+				secondary: '#304CE0',
+				action: '#E03030',
+			},
+		},
+	},
+	plugins: [],
+};
