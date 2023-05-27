1881e070533f3c0a842cbc4b476fdcaaf2a2654a
KhanhNG <khanhngn.dev@gmail.com>
2023-05-27 17:17:07 +0700

    chore(auth): update axios to check for refresh

---
+++ src/hooks/useEmptyContainers.tsx
@@ -0,0 +1,60 @@
+import { FolderOption, LockerOption } from '@/types/config';
+import { GetEmptyContainersResponse } from '@/types/response';
+import axiosClient from '@/utils/axiosClient';
+import { useQuery } from 'react-query';
+import { useMemo } from 'react';
+
+interface IUserEmptyContainers {
+	roomId: string;
+	page?: number;
+	size?: number;
+}
+
+const useEmptyContainers = ({ roomId, page = 1, size = 20 }: IUserEmptyContainers) => {
+	const { data: emptyContainers, refetch: containerRefetch } = useQuery(
+		'imports',
+		async () =>
+			(
+				await axiosClient.post<GetEmptyContainersResponse>('/rooms/empty-containers', {
+					roomId,
+					page,
+					size,
+				})
+			).data,
+		{
+			enabled: false,
+		}
+	);
+
+	const availableLockers: LockerOption[] | undefined = useMemo(
+		() =>
+			emptyContainers?.data.items.map((locker) => ({
+				name: locker.name,
+				id: locker.id,
+				free: locker.numberOfFreeFolders,
+				max: locker.numberOfFolders,
+				description: locker.description,
+			})),
+		[emptyContainers?.data.items]
+	);
+	// [{name: 'Locker 1', id: '1', max: 60, current: 40}
+
+	const availableFolders: { [key: string]: FolderOption[] } | undefined = useMemo(
+		() =>
+			emptyContainers?.data.items.reduce((acc, locker) => {
+				const folders = locker.folders.map((folder) => ({
+					name: folder.name,
+					id: folder.id,
+					free: folder.slot,
+					max: folder.capacity,
+					description: folder.description,
+				}));
+				return { ...acc, [locker.id]: folders };
+			}, {} as { [key: string]: FolderOption[] }),
+		[emptyContainers?.data.items]
+	);
+	// [{name: 'Folder 1', id: '1', max: 60, current: 40}
+	return { availableLockers, availableFolders, containerRefetch };
+};
+
+export default useEmptyContainers;
